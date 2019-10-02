8029016479a5282ba8821123ead633dd01ce10b7
shriharishastry <shriharishastrysmg@gmail.com>
2019-10-03 01:17:36 +0530

    feat(product) Adds product API

--- brightpearl/api.py
+++ brightpearl/api.py
@@ -1,37 +1,71 @@
-import requests
-from urllib.parse import urlencode
 import logging
+import uuid
+import sys
+
+from urllib.parse import urlencode
+
+from brightpearl.connection import Connection, OauthConnection
+from brightpearl.resources import Products
+
 
 log = logging.getLogger("brightpearl.api")
 
 
 class BrightPearlAPI(object):
-    def __init__(self, client_id, account, client_secret=None, redirect=None):
-        """
-            Method to initialise BrightPearlAPI Class
-        :param client_id: (string) - Apps clientId on Brightpearl Portal
-        :param account: (string) - Account Name which is installing app.
-        :param client_secret: (string) - Apps client secret.
-        :param redirect: (string) - Redirect Url
-        """
+    def __init__(
+            self, client_id=None, client_secret=None, oauth=False, account_id=None, region=None, access_token=None,
+            developer_ref=None, app_ref=None
+    ):
         self.client_id = client_id
         self.client_secret = client_secret
-        self.account = account
-        self.redirect = redirect
+        self.account = account_id
+        if oauth:
+            self.connection = OauthConnection(self.client_id, self.client_secret)
+        else:
+            self.connection = Connection(region, account_id, access_token, developer_ref, app_ref)
 
-    def oauth_fetch_token(self, code, redirect_uri):
+    def authorization_url(self, authorization_redirect_url):
+        """
+            Method to generate the Authorization url.
+        :return: (string) - url to be called for getting authorization token.
+        """
+        query_params = dict({
+            "response_type": "code",
+            "client_id": self.client_id,
+            "redirect_uri": authorization_redirect_url,
+            "state": "random"
+        })
+        return "https://oauth.brightpearl.com/authorize/{}?{}".format(self.account, urlencode(query_params))
+
+    def oauth_fetch_token(self, code, access_redirect_url):
         """
             Method to get access token from the Authorization Code.
         :param code: (string) - Authorization code returned by Brightpearl upon authorization.
-        :param redirect_uri: (string)
+        :param access_redirect_url: (string)
         :return: Brightpearl response Object
         """
-        request_param = {
+        request_body = dict({
             "grant_type": "authorization_code",
             "code": code,
             "client_id": self.client_id,
-            "redirect_uri": redirect_uri
-        }
-        url = "https://oauth.brightpearl.com/token/{}".format(self.account)
-        log.debug("Calling " + url)
-        return requests.post(url, data=request_param)
+            "redirect_uri": access_redirect_url
+        })
+        return self.connection.make_request(
+            "https://oauth.brightpearl.com/token/{}".format(self.account), "POST", request_body
+        )
+
+    def __getattr__(self, item):
+        return ResourceWrapper(item, self.connection)
+
+
+class ResourceWrapper(object):
+    def __init__(self, resource_cls, conn_obj):
+        self.conn_obj = conn_obj
+        self.resource_class = self.str_to_class(resource_cls)
+
+    def __getattr__(self, item):
+        return lambda *args, **kwargs: (getattr(self.resource_class(self.conn_obj), item))(*args, **kwargs)
+
+    @classmethod
+    def str_to_class(cls, resource):
+        return getattr(sys.modules[__name__], resource)
---
+++ brightpearl/resources/products.py
@@ -0,0 +1,38 @@
+
+
+class Products(object):
+
+    def __init__(self, connection):
+        self.resource_parent = 'product-service'
+        self.connection = connection
+
+    def all(self):
+        product_list = "product-search"
+        return self.connection.make_request("/{}/{}".format(
+            self.resource_parent, product_list), "GET", {}
+        )
+
+    def get(self, product_id):
+        product_get = "product"
+        return self.connection.make_request(
+            "{}/{}/{}".format(self.resource_parent, product_get, product_id), "GET", {}
+        )
+
+    def create(self, product_info):
+        product_create = "product"
+        return self.connection.make_request(
+            "{}/{}".format(self.resource_parent, product_create), "PUT", data=product_info
+        )
+
+    def update(self, product_info):
+        product_update = "product"
+        return self.connection.make_request(
+           "{}/{}/{}".format(self.resource_parent, product_update, product_info['id']), "PUT", data=product_info
+        )
+
+    def remove(self, product_id):
+        product_delete = "product"
+        return self.connection.make_request(
+            "{}/{}/{}".format(self.resource_parent, product_delete, product_id), "DELETE", {}, {}
+        )
+

316f5f40c0649acd34a4b6ed3612b281c55d77e9
shriharishastry <shriharishastrysmg@gmail.com>
2019-11-28 19:52:08 +0530

    fix (rate limit): fixes key error om headers

--- brightpearl/resources/brands.py
+++ brightpearl/resources/brands.py
@@ -24,6 +24,11 @@ class Brands(object):
         )
 
     def get(self, brand_id):
+        """
+            Method to get the brand information for a brand with <brand_id>
+        :param brand_id: (string) - Brand id for which information is needed.
+        :return:
+        """
         brand_get = "brand"
         return self.connection.make_request(
             "{}/{}/{}".format(self.resource_parent, brand_get, brand_id), "GET", {}
@@ -36,12 +41,22 @@ class Brands(object):
         )
 
     def update(self, brand_info):
+        """
+            Method to update the brand information
+        :param brand_info: (dict) - Brand information dictionary.
+        :return:
+        """
         brand_update = "brand"
         return self.connection.make_request(
             "{}/{}/{}".format(self.resource_parent, brand_update, brand_info['id']), "PUT", data=brand_info
         )
 
     def remove(self, brand_id):
+        """
+            Method to delete the brand from the brightpearl
+        :param brand_id: (String) - Brand Id which is supposed to be deleted.
+        :return:
+        """
         brand_delete = "brand"
         return self.connection.make_request(
             "{}/{}/{}".format(self.resource_parent, brand_delete, brand_id), "DELETE", {}
